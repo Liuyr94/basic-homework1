36277e9322ec2c5fbac0a083cd0cae6c848b1183
Henadzi Bessonov <bessonov.henadzi@gmail.com>
2023-10-17 15:26:31 +0300

    homework - "rework".

--- src/main/java/com/bessonov/homeworks/MaiTest.java
+++
@@ -1,8 +0,0 @@
-package com.bessonov.homeworks;
-
-public class MaiTest {
-    public static void main(String[] args) {
-        int dataRandom = (int) ((Math.random() * (21 - 21)) + 21);
-        System.out.println(dataRandom);
-    }
-}
